98b19bf22ca451c1d9fc9cc1e50fbdb3b806e024
Gradon <gradon.kam@gmail.com>
2020-10-16 19:23:33 -0500

    change from using a for loop to build the means to a simple summary
    add changes to the variable names in the dataset for readability

--- run_analysis.R
+++ run_analysis.R
@@ -3,9 +3,9 @@
 
 ## assume that the data is contained in a subdirectory called "UCI HAR Dataset".
 
+library(dplyr)
+
 ## read the data from the test dataset and training dataset into R tables
-## each observation contains 561 variables, stored in X_test or X_train
-## the activity tracked for each observation is stored in Y_test or Y_train
 test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 
@@ -20,71 +20,48 @@ trainsubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 ## pull labels for what each activity 'number' means from activity_labels.txt
 activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
+## read the variable names from features.txt
+varnames <- read.table("./UCI HAR Dataset/features.txt")
+
+## set names of the variables in test and train datasets
+names(test) <- varnames$V2
+names(train) <- varnames$V2
+
 ## replace the numbered activity names with their labels (sort of ugly)
 testactivities <- merge(testactivities, activitylabels, by = "V1", sort = FALSE)
 trainactivities <- merge(trainactivities, activitylabels, by = "V1", sort = FALSE)
 testactivities <- testactivities[,2]
 trainactivities <- trainactivities[,2]
 
-testsubjects <- rename(testsubjects, subject = V1)
-trainsubjects <- rename(trainsubjects, subject = V1)
+## add the activity and subjects tracked as a column to each dataset
+test <- cbind(activity = testactivities, subject = testsubjects$V1, test)
+train <- cbind(activity = trainactivities, subject = trainsubjects$V1, train)
 
-## read the variable names from features.txt
-## this table will have 561 rows corresponding to the variables in the datasets
-varnames <- read.table("./UCI HAR Dataset/features.txt")
-
-## names of the variables are in the second column of the features.txt table
-namesvector <- varnames[,2]
-
-## set names of the variables in test and train datasets
-names(test) <- namesvector
-names(train) <- namesvector
-
-## add the activity tracked as a column to each dataset
-test <- cbind(test, activity = testactivities)
-train <- cbind(train, activity = trainactivities)
-
-## add the subject identifies as a column to each dataset
-test <- cbind(test, subject = testsubjects)
-train <- cbind(train, subject = trainsubjects)
-
-## combine the HAR data test and training datasets into a single table
+## combine the test and training datasets into a single table
 combinedata <- rbind(test, train)
 
 ## extract the mean and stdev measurements and the activity/subject identifiers
-extractdata <- select(combinedata, contains("mean()") | contains ("std()") | contains ("activity") | contains("subject"))
+combinedata <- select(combinedata, activity | subject | contains("mean()") | contains ("std()"))
 
-## save the variable names since we'll be making a new data frame
-namesvector <- names(extractdata)
+## make names readable
+names(combinedata) <- gsub("^t", "Time", names(combinedata))
+names(combinedata) <- gsub("^f", "Frequency", names(combinedata))
+names(combinedata) <- gsub("Acc", "Acceleration", names(combinedata))
+names(combinedata) <- gsub("Gyro", "Gyroscope", names(combinedata))
+names(combinedata) <- gsub("Mag", "Magnitude", names(combinedata))
+names(combinedata) <- gsub("-mean", "Mean", names(combinedata))
+names(combinedata) <- gsub("-std", "StdDev", names(combinedata))
+names(combinedata) <- gsub("-", "", names(combinedata))
+names(combinedata) <- gsub("\\(", "", names(combinedata))
+names(combinedata) <- gsub("\\)", "", names(combinedata))
 
-## create a new independent tidy data set with the average of each variable for each activity and subject
-## start by splitting the data frame into a list of data frames for each activity/subject combo
-splitframe <- split(extractdata, list(extractdata$activity, extractdata$subject))
+## build data frame to perform mean over each activity/subject
 
-## for each activity/subject combination there will be a row of our new data frame
-## iterate over the list of data frames corresponding to each subject/activity
-## not every subject has data for every activity
-tempframe <- data.frame()
-tempframe2 <- data.frame()
-newdata <- data.frame()
-for(i in 1:length(splitframe))
-{
-    tempframe <- as.data.frame(splitframe[i])
-    if(nrow(tempframe) > 0)
-    {
-      ## store the activity and subject to the side, don't need their mean
-      currentactivity <- tempframe[1,67]
-      currentsubject <- tempframe[1,68]
-      tempframe <- select(tempframe, 1:66)
-      ## iterate over the variables and put their mean into a new frame
-      for(j in 1:ncol(tempframe))
-      {
-          tempframe2[1,j] <- mean(tempframe[,j])
-      }
-      tempframe2[1,67] <- currentactivity
-      tempframe2[1,68] <- currentsubject
-      ## tempframe2 should be a single-row data frame with the means of each variable
-      newdata <- rbind(newdata, tempframe2)
-    }
-}
-names(newdata) <- namesvector
+## create grouping by subject and activity
+combinedata <- group_by(combinedata, subject, activity)
+
+## apply mean to the combined data using the sorted groupings
+finishdata <- summarize_all(combinedata, .funs = mean)
+
+## write tidy data set to file
+write.table(finishdata, "./tidydata.txt")
